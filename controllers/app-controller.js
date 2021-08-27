5bcd56d9746c6300ff328c61713e7acea35822e8
github username <d24dust@hotmail.com>
2021-08-27 15:17:27 -0400

    add: action invoke code

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -5,6 +5,14 @@ class AppController {
     this.action = action
     this.params = req.body
     this.response = res
+
+    try {
+      this[action]()
+    } catch (error) {
+      this.response.status = 404
+      this.response.send({error: "Invalid Action"})
+      throw Error("InvalidActionForController> Action not defined/not found")
+    }
   }
 }
 
