2e8724239e70d9a4846bdce668fb1840b7491dc4
github username <d24dust@hotmail.com>
2021-08-27 15:16:37 -0400

    add: constructor with properties

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -1,5 +1,11 @@
 class AppController {
 
+  constructor(req, res, action) {
+    this.method = req.method
+    this.action = action
+    this.params = req.body
+    this.response = res
+  }
 }
 
 export default AppController
