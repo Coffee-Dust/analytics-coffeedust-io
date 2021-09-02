6ae76c141d4c9daf8d3dcee851cc2e6de828ef42
github username <d24dust@hotmail.com>
2021-09-02 14:28:04 -0400

    feat: create action

--- controllers/report-controller.js
+++ controllers/report-controller.js
@@ -2,6 +2,18 @@ import AppController from "./app-controller.js"
 
 class ReportController extends AppController {
 
+  create() {
+
+    const report = Report.build(this.params)
+    report.save()
+    .catch(error=> {
+      this.response.status(569).send({error: true, backtrace: error})
+    })
+    .then(_=> {
+      this.response.send(report)
+    })
+  }
+
 }
 
 export default ReportController
