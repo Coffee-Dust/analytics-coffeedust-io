3200165a0040744344ce477778ea7775d0eb3217
github username <d24dust@hotmail.com>
2021-09-02 14:24:02 -0400

    add: CORS functionality

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const app = express()
 const port = 420
 app.use(express.json());
 
+app.use(function (req, res, next) {
+
+  const allowedOrigins = [
+    "https://coffeedust.io",
+    "http://localhost:3000"
+  ]
+
+  if (allowedOrigins.includes(req.headers.origin)) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin)
+    res.header('Access-Control-Allow-Methods', 'POST')
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  }
+  next()
+})
+
 app.get('/', (req, res) => {
   res.redirect("https://coffeedust.io/")
 })
