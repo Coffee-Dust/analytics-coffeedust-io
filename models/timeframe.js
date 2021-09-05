a58b039725cbee10ff27ebea6a905907a952547a
github username <d24dust@hotmail.com>
2021-09-05 14:45:29 -0400

    add: getter/setter methods for .from

--- models/timeframe.js
+++ models/timeframe.js
@@ -1,7 +1,17 @@
+import moment from "moment"
+
 class Timeframe {
-  
+
   constructor(timeframeKeyword) {
 
+  }
+
+  get from() {
+    return this._from
+  }
+
+  set from(dateTime) {
+    this._from = new Date(dateTime)
   }
 
 }
