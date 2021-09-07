e90246a97b0fb68f2332486d135acc74b7585264
github username <d24dust@hotmail.com>
2021-09-07 19:31:25 -0400

    add: queryMethod for UniqueVisitsAndInteractionCount

--- models/report-collection.js
+++ models/report-collection.js
@@ -3,6 +3,17 @@ import sequelize from "../db/sequelize-config.js";
 class ReportCollection {
   // All property setting query method names must start with 'query'
 
+  async queryUniqueVisitsAndInteractionCount() {
+    const [data, _] = await sequelize.query(`
+    SELECT ip, COUNT(eventType) as 'interactionCount' FROM 'Reports' 
+    WHERE createdAt BETWEEN '${this.timeframe.from}' AND '${this.timeframe.to}' 
+    GROUP BY ip;`)
+
+    this.uniqueVisits = data.length
+    const sum = data.reduce((prev, curr)=> prev.interactionCount || 0 + curr.interactionCount, 0)
+    this.interactionAverage = sum / this.uniqueVisits
+  }
+
   async runQueryStatisticDataMethods() {
     // Automatic method invoker for instance methods that start with 'query'
     for (const methodName of Object.getOwnPropertyNames(this.constructor.prototype)) {
