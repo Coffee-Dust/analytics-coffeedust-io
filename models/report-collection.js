44a2df6df8eea7f25239d9006e3f6e1f936ff0a3
github username <d24dust@hotmail.com>
2021-09-07 19:29:23 -0400

    add/feat: async query method invoker method

--- models/report-collection.js
+++ models/report-collection.js
@@ -1,6 +1,14 @@
 import sequelize from "../db/sequelize-config.js";
 
 class ReportCollection {
+  // All property setting query method names must start with 'query'
+
+  async runQueryStatisticDataMethods() {
+    // Automatic method invoker for instance methods that start with 'query'
+    for (const methodName of Object.getOwnPropertyNames(this.constructor.prototype)) {
+      if (methodName.slice(0, 5) === "query") { await this[methodName]() }
+    }
+  }
 
   constructor(timeframe) {
     this.timeframe = timeframe
