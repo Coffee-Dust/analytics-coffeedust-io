2da5d8cc08c19c0b332d5d3b2d64b4d5fd3e7f2a
github username <d24dust@hotmail.com>
2021-09-07 19:35:20 -0400

    add: class ORM method for Timeframe queries

--- models/report.js
+++ models/report.js
@@ -2,6 +2,12 @@ import { Sequelize } from "sequelize"
 
 class Report extends Sequelize.Model {
 
+  static async findAllDataForTimeframe(timeframe) {
+    return this.findAll({where: {
+      createdAt: {[Sequelize.Op.between]: [timeframe.from, timeframe.to]}
+    }})
+  }
+
 }
 
 export default Report
