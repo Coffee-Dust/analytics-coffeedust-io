f93dc78e80d99225ed8a197aa11de7171ec1a7ca
github username <d24dust@hotmail.com>
2021-09-07 17:14:46 -0400

    add: checkReqAPIKey() method

--- controllers/stats-controller.js
+++ controllers/stats-controller.js
@@ -4,6 +4,12 @@ class StatsController extends AppController {
 
 
 
+  checkReqAPIKey() {
+    if (this.headers.apikey !== process.env.ANALYTICS_CD_IO_STATS_API_KEY) {
+      this.response.status = 403
+      throw new Error("<InvalidApiKey> The header apikey is missing or incorrect!")
+    }
+  }
 }
 
 export default StatsController
