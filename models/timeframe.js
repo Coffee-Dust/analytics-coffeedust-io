27cae4a3cae3cfa5eafbc6d2827290d8cd3c36dd
github username <d24dust@hotmail.com>
2021-09-07 19:32:31 -0400

    implement: constructor

--- models/timeframe.js
+++ models/timeframe.js
@@ -3,7 +3,14 @@ import moment from "moment"
 class Timeframe {
 
   constructor(timeframeKeyword) {
-
+    switch (timeframeKeyword) {
+      case "today":
+        this.from = moment().startOf('day')
+        this.to = moment().endOf('day')
+        break;
+      default:
+        throw new Error("<UnknownTimeframeKeyword> Unknown or invalid timeframe keyword was provided to Timeframe instance.")
+    }
   }
 
   get from() {
