bdf3fbe3df0278aea3c2bf3a4b101bdda4a5393b
github username <d24dust@hotmail.com>
2021-09-07 17:16:30 -0400

    add: index action

--- controllers/stats-controller.js
+++ controllers/stats-controller.js
@@ -4,7 +4,11 @@ import Timeframe from "../models/timeframe.js"
 
 class StatsController extends AppController {
 
-
+  async index() {
+    this.checkReqAPIKey()
+    const response = await ReadableResponse.generateForFormat(this.params.dataFormat, new Timeframe(this.params.timeframe))
+    this.response.send(response)
+  }
 
   checkReqAPIKey() {
     if (this.headers.apikey !== process.env.ANALYTICS_CD_IO_STATS_API_KEY) {
